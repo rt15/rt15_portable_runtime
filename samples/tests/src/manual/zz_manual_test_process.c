4c57112213bb279270e8a6c805d8ee35ca966ada
rt15 <boissard.bruno@gmail.com>
2022-11-27 21:30:59 +0100

    Simplify zz_manual_test_process

--- samples/tests/src/manual/zz_manual_test_process.c
+++ samples/tests/src/manual/zz_manual_test_process.c
@@ -2,9 +2,8 @@
 
 rt_s zz_manual_test_process()
 {
-	struct rt_process process;
-	rt_b process_created = RT_FALSE;
 	const rt_char *application_path_and_args[5];
+	rt_un32 exit_code;
 	rt_s ret;
 
 	application_path_and_args[0] = _R("ping");
@@ -19,33 +18,19 @@ rt_s zz_manual_test_process()
 	application_path_and_args[3] = _R("3");
 	application_path_and_args[4] = RT_NULL;
 
-	if (!rt_console_write_string(_R("==========================")))
-		goto error;
+	if (!rt_console_write_string(_R("=========================="))) goto error;
 
 #ifdef RT_DEFINE_LINUX
-	if (!rt_console_write_string_with_size(_R("\n"), 1))
-		goto error;
+	if (!rt_console_write_string_with_size(_R("\n"), 1)) goto error;
 #endif
 
-	if (!rt_process_create(&process, RT_TRUE, RT_NULL, RT_NULL, application_path_and_args))
-		goto error;
-	process_created = RT_TRUE;
+	if (!rt_process_spawn_sync(RT_NULL, RT_NULL, application_path_and_args, &exit_code)) goto error;
+	if (!rt_console_write_string(_R("==========================\n"))) goto error;
+	if (!rt_console_write_string(_R("Joined!\n"))) goto error;
+	if (exit_code) goto error;
 
 	ret = RT_OK;
 free:
-	if (process_created) {
-		if (!rt_process_join(&process) && ret)
-			goto error;
-
-		if (!rt_console_write_string(_R("==========================\n")) && ret)
-			goto error;
-		if (!rt_console_write_string(_R("Joined!\n")) && ret)
-			goto error;
-
-		process_created = RT_FALSE;
-		if (!rt_process_free(&process) && ret)
-			goto error;
-	}
 	return ret;
 error:
 	ret = RT_FAILED;
