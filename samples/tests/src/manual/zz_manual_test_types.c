4cbc27693449bb3ac70d989b970b90fb1d4553b3
rt15 <boissard.bruno@gmail.com>
2022-12-02 22:43:53 +0100

    Display page size

--- samples/tests/src/manual/zz_manual_test_types.c
+++ samples/tests/src/manual/zz_manual_test_types.c
@@ -71,12 +71,35 @@ error:
 	goto free;
 }
 
+static rt_s zz_manual_test_types_display_page_size()
+{
+	rt_un page_size;
+	rt_char buffer[RT_CHAR_QUARTER_BIG_STRING_SIZE];
+	rt_un buffer_size;
+	rt_s ret;
+
+	page_size = rt_virtual_memory_get_page_size();
+	buffer_size = 12;
+	if (!rt_char_copy(_R("Page size = "), buffer_size, buffer, RT_CHAR_QUARTER_BIG_STRING_SIZE)) goto error;
+	if (!rt_char_append_un(page_size, 10, buffer, RT_CHAR_QUARTER_BIG_STRING_SIZE, &buffer_size)) goto error;
+	if (!rt_char_append_char(_R('\n'), buffer, RT_CHAR_QUARTER_BIG_STRING_SIZE, &buffer_size)) goto error;
+	if (!rt_console_write_string_with_size(buffer, buffer_size)) goto error;
+
+	ret = RT_OK;
+free:
+	return ret;
+error:
+	ret = RT_FAILED;
+	goto free;
+}
+
 rt_s zz_manual_test_types()
 {
 	rt_s ret;
 
 	if (!zz_manual_test_types_display_flags()) goto error;
 	if (!zz_manual_test_types_display_signedness()) goto error;
+	if (!zz_manual_test_types_display_page_size()) goto error;
 
 	ret = RT_OK;
 free:
