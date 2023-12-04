f68b931102de5fa613fc7b8658ccd133574b20d8
rt15 <boissard.bruno@gmail.com>
2023-12-04 10:15:57 +0100

    Remove Code::Blocks files

--- clean.bat
+++ clean.bat
@@ -1,15 +1,10 @@
 @echo off
 
-del /Q project\*.layout
-
 rmdir /S /Q rpr\obj\Release
 rmdir /S /Q rpr\obj\Debug
-del /S /Q rpr\lib\*.a
-del /S /Q rpr\lib\*.lib
-del /S /Q rpr\lib\*.pdb
+rmdir /S /Q rpr\build
+del /Q rpr\lib\*rpr*
 rmdir /S /Q rpr\project\.vs
-del /Q rpr\project\*.layout
-del /Q rpr\project\*.depend
 del /Q rpr\project\*.user
 
 for /D %%I IN (tests tests_with_crt) DO call :clean_project %%I
@@ -20,7 +15,7 @@ goto :eof
 
 rmdir /S /Q samples\%1\obj\Release
 rmdir /S /Q samples\%1\obj\Debug
-del /Q samples\%1\bin\%1*
-del /Q samples\%1\project\*.layout
-del /Q samples\%1\project\*.depend
-
+rmdir /S /Q samples\%1\build
+del /Q samples\%1\bin\*%1*
+rmdir /S /Q samples\%1\project\*.vs
+del /Q samples\%1\project\*.user
