a35af990d6be28e402e6e6cf9594e840c8cb31e1
rt15 <boissard.bruno@gmail.com>
2024-07-28 23:08:37 +0200

    Clean rt_quick_sort

--- rpr/src/layer002/rt_quick_sort.c
+++ rpr/src/layer002/rt_quick_sort.c
@@ -3,292 +3,201 @@
 #include "layer001/rt_memory.h"
 
 /**
- * shortsort(hi, lo, width, comp, context) - insertion sort for sorting short arrays
- *
- * Purpose:
- *   sorts the sub-array of elements between lo and hi (inclusive)
- *   side effects: sorts in place
- *   assumes that lo < hi
- *
- * Entry:
- *   char *lo = pointer to low element to sort
- *   char *hi = pointer to high element to sort
- *   size_t width = width in bytes of each array element
- *   int (*comp)() = pointer to function returning analog of strcmp for
- *   strings, but supplied by user for comparing the array elements.
- *   it accepts 2 pointers to elements, together with a pointer to a context
- *   (if present). Returns neg if 1<2, 0 if 1=2, pos if 1>2.
- *   void *context - pointer to the context in which the function is
- *   called. This context is passed to the comparison function.
- *
+ * Selection sort should be better in our case as it should swap less items than insertion sort.
+ * 
+ * <p>
+ * We put the max of the unsorted array as the last element, decrease the size of the unsorted array, then repeat.
+ * </p>
  */
-static rt_s rt_quick_sort_short_sort(char *lo, char *hi, rt_un item_size, rt_comparison_callback_t callback, void *context)
+static rt_s rt_quick_sort_selection_sort(rt_char8 *low, rt_char8 *high, rt_un item_size, rt_comparison_callback_t callback, void *context)
 {
-	char *p, *max;
+	rt_char8 *end_of_unsorted = high;
+	rt_char8 *max;
+	rt_char8 *in_unsorted;
 	rt_n comparison_result;
+	rt_s ret;
 
-	/* Note: in assertions below, i and j are alway inside original bound of array to sort. */
-	while (hi > lo) {
-		/* A[i] <= A[j] for i <= j, j > hi */
-		max = lo;
-		for (p = lo+item_size; p <= hi; p += item_size) {
-			/* A[i] <= A[max] for lo <= i < p */
-
-			if (!callback(p, max, context, &comparison_result)) {
-				return RT_FAILED;
-			}
-			if (comparison_result > 0) {
-				max = p;
-			}
-			/* A[i] <= A[max] for lo <= i <= p */
+	/* While the unsorted array has at least two elements (low and end_of_unsorted). */
+	while (end_of_unsorted > low) {
+		/* We search for the max of the unsorted array. */
+		max = low;
+		for (in_unsorted = low + item_size; in_unsorted <= end_of_unsorted; in_unsorted += item_size) {
+			if (RT_UNLIKELY(!callback(in_unsorted, max, context, &comparison_result)))
+				goto error;
+			if (comparison_result > 0)
+				max = in_unsorted;
 		}
 
-		/* A[i] <= A[max] for lo <= i <= hi */
+		/* We put the maximum of the unsorted array at the end of the unsorted array. */
+		rt_memory_swap(max, end_of_unsorted, item_size);
 
-		rt_memory_swap(max, hi, item_size);
-
-		/* A[i] <= A[hi] for i <= hi, so A[i] <= A[j] for i <= j, j >= hi */
-
-		hi -= item_size;
-
-		/* A[i] <= A[j] for i <= j, j > hi, loop top condition established */
+		/* The last item of the unsorted array will be now part of the sorted array at the end. */
+		end_of_unsorted -= item_size;
 	}
-	/* A[i] <= A[j] for i <= j, j > lo, which implies A[i] <= A[j] for i < j, so array is sorted */
-	return RT_OK;
+
+	ret = RT_OK;
+free:
+	return ret;
+
+error:
+	ret = RT_FAILED;
+	goto free;
 }
 
 /**
- * qsort(base, num, wid, comp) - quicksort function for sorting arrays
- *
- * Purpose:
- *   quicksort the array of elements
- *   side effects: sorts in place
- *   maximum array size is number of elements times size of elements,
- *   but is limited by the virtual address space of the processor
- *
- * Entry:
- *   char *base = pointer to base of array
- *   size_t num = number of elements in the array
- *   size_t width = width in bytes of each array element
- *   int (*comp)() = pointer to function returning analog of strcmp for
- *   strings, but supplied by user for comparing the array elements.
- *   it accepts 2 pointers to elements.
- *   Returns neg if 1<2, 0 if 1=2, pos if 1>2.
- *
+ * Partition the data into 2 parts. At the end:<br>
+ * From low to pivot, all the values are less or equals to the value pointed by pivot.<br>
+ * From pivot to high, all the values are greater or equals to the value pointed by pivot.
  */
+static rt_s rt_quick_sort_partition(rt_char8 *low, rt_char8 *high, rt_un size, rt_un item_size, rt_comparison_callback_t callback, void *context, rt_char8 **pivot)
+{
+	rt_char8 *middle = low + (size / 2) * item_size;
+	rt_n comparison_result;
+	rt_char8 *current_low;
+	rt_char8 *current_high;
+	rt_char8* local_pivot;
+	rt_s ret;
+
+	/* Sort low, middle and high items. */
+	/* As a result, the pivot is chosen from 3 values, and is stored in the middle of the array. */
+
+	if (RT_UNLIKELY(!callback(low, middle, context, &comparison_result)))
+		goto error;
+	if (comparison_result > 0)
+		rt_memory_swap(low, middle, item_size);
+	
+	if (RT_UNLIKELY(!callback(low, high, context, &comparison_result)))
+		goto error;
+	if (comparison_result > 0)
+		rt_memory_swap(low, high, item_size);
+
+	if (RT_UNLIKELY(!callback(middle, high, context, &comparison_result)))
+		goto error;
+	if (comparison_result > 0)
+		rt_memory_swap(middle, high, item_size);
+	
+	current_low = low;
+	current_high = high;
+	local_pivot = middle;
+
+	while (RT_TRUE) {
+
+		/* Search the first low that is greater than the pivot. */
+		while (RT_TRUE) {
+			if (current_low >= local_pivot)
+				 break;
+			current_low += item_size;
+			if (current_low == local_pivot)
+				break;
+			if (RT_UNLIKELY(!callback(current_low, local_pivot, context, &comparison_result)))
+				goto error;
+			if (comparison_result > 0)
+				break;
+		}
+
+		/* Search the first high that is smaller than the pivot, starting from the end. */
+		while (RT_TRUE) {
+			if (current_high <= local_pivot)
+				break;
+			current_high -= item_size;
+			if (current_high == local_pivot)
+				break;
+			if (RT_UNLIKELY(!callback(current_high, local_pivot, context, &comparison_result)))
+				goto error;
+			if (comparison_result < 0)
+				break;
+		}
+
+		/* If indexes cross, the partitioning is done. */
+		if (current_low >= current_high)
+			break;
+
+		/* Both values pointed by current_low and current_high are on the wrong side of the pivot, so we swap them. */
+		rt_memory_swap(current_low, current_high, item_size);
+
+		/* If we moved the pivot, we have to reflect this move into local_pivot pointer. */
+		if (local_pivot == current_high)
+			local_pivot = current_low;
+		else if (local_pivot == current_low)
+			local_pivot = current_high;
+	}
+	*pivot = local_pivot;
+			
+	ret = RT_OK;
+free:
+	return ret;
+
+error:
+	ret = RT_FAILED;
+	goto free;
+}
+
 rt_s rt_quick_sort(const void *area, rt_un size, rt_un item_size, rt_comparison_callback_t callback, void *context)
 {
-	/* Note: the number of stack entries required is no more than
-	   1 + log2(size), so 30 is sufficient for any array.
-	   Calculated with STKSIZ (8*sizeof(void*) - 2) = 62 for 64 bits. */
-	char *lo, *hi;			/* ends of sub-array currently sorting */
-	char *mid;			/* points to middle of subarray */
-	char *loguy, *higuy;		/* traveling pointers for partition step */
-	rt_un sub_array_size;		/* size of the sub-array */
-	char *lostk[62];
-	char *histk[62];
-	int stkptr;			/* stack for saving sub-array to be processed */
-	rt_n comparison_result;
+	rt_char8 *low = (rt_char8*)area;
+	rt_char8 *high = (rt_char8*)area + (size - 1) * item_size;
+	rt_un sub_array_size;
+	rt_char8 *pivot;
+	rt_char8 *low_stack[62];
+	rt_char8 *high_stack[62];
+	rt_un stacks_size = 0;
+	rt_s ret;
 
-	if (size < 2)
-		return RT_OK; /* nothing to do */
-
-	stkptr = 0; /* initialize stack */
-
-	lo = (char *)area;
-	hi = (char *)area + item_size * (size - 1); /* initialize limits */
-
-	/* this entry point is for pseudo-recursion calling: setting
-	   lo and hi and jumping to here is like recursion, but stkptr is
-	   preserved, locals aren't, so we preserve stuff on the stack */
 recurse:
 
-	sub_array_size = (rt_un)((hi - lo) / item_size + 1); /* number of el's to sort */
+	if (high != low) {
+		sub_array_size = (high - low) / item_size + 1;
 
-	/* below a certain sub_array_size, it is faster to use a O(n^2) sorting method */
-	if (sub_array_size <= 8) {
-		if (!rt_quick_sort_short_sort(lo, hi, item_size, callback, context)) {
-			return RT_FAILED;
-		}
-	} else {
-		/* First we pick a partitioning element. The efficiency of the
-		   algorithm demands that we find one that is approximately the median
-		   of the values, but also that we select one fast. We choose the
-		   median of the first, middle, and last elements, to avoid bad
-		   performance in the face of already sorted data, or data that is made
-		   up of multiple sorted runs appended together. Testing shows that a
-		   median-of-three algorithm provides better performance than simply
-		   picking the middle element for the latter case. */
+		if (sub_array_size <= 8) {
+	
+			if (RT_UNLIKELY(!rt_quick_sort_selection_sort(low, high, item_size, callback, context)))
+				goto error;
 
-		mid = lo + (sub_array_size / 2) * item_size; /* find middle element */
+		} else {
 
-		/* Sort the first, middle, last elements into order */
+			if (RT_UNLIKELY(!rt_quick_sort_partition(low, high, sub_array_size, item_size, callback, context, &pivot)))
+				goto error;
 
-		if (!callback(lo, mid, context, &comparison_result)) {
-			return RT_FAILED;
-		}
-		if (comparison_result > 0) {
-			rt_memory_swap(lo, mid, item_size);
-		}
-		if (!callback(lo, hi, context, &comparison_result)) {
-			return RT_FAILED;
-		}
-		if (comparison_result > 0) {
-			rt_memory_swap(lo, hi, item_size);
-		}
-		if (!callback(mid, hi, context, &comparison_result)) {
-			return RT_FAILED;
-		}
-		if (comparison_result > 0) {
-			rt_memory_swap(mid, hi, item_size);
-		}
-
-		/* We now wish to partition the array into three pieces, one consisting
-		   of elements <= partition element, one of elements equal to the
-		   partition element, and one of elements > than it. This is done
-		   below; comments indicate conditions established at every step. */
-
-		loguy = lo;
-		higuy = hi;
-
-		/* Note that higuy decreases and loguy increases on every iteration, so loop must terminate. */
-		for (;;) {
-			/* lo <= loguy < hi, lo < higuy <= hi,
-			   A[i] <= A[mid] for lo <= i <= loguy,
-			   A[i] > A[mid] for higuy <= i < hi,
-			   A[hi] >= A[mid] */
-
-			/* The doubled loop is to avoid calling comp(mid,mid), since some
-			   existing comparison funcs don't work when passed the same
-			   value for both pointers. */
-
-			if (mid > loguy) {
-				while (RT_TRUE)	{
-					loguy += item_size;
-					if (loguy >= mid) break;
-					if (!callback(loguy, mid, context, &comparison_result)) return RT_FAILED;
-					if (comparison_result > 0) break;
+			/* We do the smaller partition first to limit the stack size. */
+			if (pivot - low >= high - pivot) {
+				/* We save the bigger partition (the lower one) for later. */
+				if (low < pivot) {
+					low_stack[stacks_size] = low;
+					high_stack[stacks_size] = pivot - item_size;
+					stacks_size++;
 				}
-			}
-			if (mid <= loguy) {
-				while (RT_TRUE)	{
-					loguy += item_size;
-					if (loguy > hi) break;
-					if (!callback(loguy, mid, context, &comparison_result)) return RT_FAILED;
-					if (comparison_result > 0) break;
+				if (high > pivot) {
+					low = pivot + item_size;
+					goto recurse;
 				}
-			}
-
-			/* lo < loguy <= hi+1, A[i] <= A[mid] for lo <= i < loguy,
-			   either loguy > hi or A[loguy] > A[mid] */
-
-			while (RT_TRUE) {
-				higuy -= item_size;
-				if (higuy <= mid) break;
-				if (!callback(higuy, mid, context, &comparison_result)) return RT_FAILED;
-				if (comparison_result <= 0) break;
-			}
-
-			/* lo <= higuy < hi, A[i] > A[mid] for higuy < i < hi,
-			   either higuy == lo or A[higuy] <= A[mid] */
-
-			if (higuy < loguy)
-				break;
-
-			/* if loguy > hi or higuy == lo, then we would have exited, so
-			   A[loguy] > A[mid], A[higuy] <= A[mid],
-			   loguy <= hi, higuy > lo */
-
-			rt_memory_swap(loguy, higuy, item_size);
-
-			/* If the partition element was moved, follow it. Only need
-			   to check for mid == higuy, since before the swap,
-			   A[loguy] > A[mid] implies loguy != mid. */
-
-			if (mid == higuy)
-				mid = loguy;
-
-			/* A[loguy] <= A[mid], A[higuy] > A[mid]; so condition at top
-			   of loop is re-established */
-		}
-
-		/* A[i] <= A[mid] for lo <= i < loguy,
-		   A[i] > A[mid] for higuy < i < hi,
-		   A[hi] >= A[mid]
-		   higuy < loguy
-		   implying:
-		   higuy == loguy-1
-		   or higuy == hi - 1, loguy == hi + 1, A[hi] == A[mid] */
-
-		/* Find adjacent elements equal to the partition element. The
-		   doubled loop is to avoid calling comp(mid,mid), since some
-		   existing comparison funcs don't work when passed the same value
-		   for both pointers. */
-
-		higuy += item_size;
-		if (mid < higuy) {
-			do {
-				higuy -= item_size;
-				if (higuy <= mid) break;
-				if (!callback(higuy, mid, context, &comparison_result)) return RT_FAILED;
-			} while (comparison_result == 0);
-		}
-		if (mid >= higuy) {
-			do {
-				higuy -= item_size;
-				if (higuy <= lo) break;
-				if (!callback(higuy, mid, context, &comparison_result)) return RT_FAILED;
-			} while (comparison_result == 0);
-		}
-
-		/* OK, now we have the following:
-		   higuy < loguy
-		   lo <= higuy <= hi
-		   A[i] <= A[mid] for lo <= i <= higuy
-		   A[i] == A[mid] for higuy < i < loguy
-		   A[i] > A[mid] for loguy <= i < hi
-		   A[hi] >= A[mid] */
-
-		/* We've finished the partition, now we want to sort the subarrays
-		   [lo, higuy] and [loguy, hi].
-		   We do the smaller one first to minimize stack usage.
-		   We only sort arrays of size 2 or more.*/
-
-		if ( higuy - lo >= hi - loguy ) {
-			if (lo < higuy) {
-				lostk[stkptr] = lo;
-				histk[stkptr] = higuy;
-				++stkptr;
-			} /* save big recursion for later */
-
-			if (loguy < hi) {
-				lo = loguy;
-				goto recurse; /* do small recursion */
-			}
-		}
-		else {
-			if (loguy < hi) {
-				lostk[stkptr] = loguy;
-				histk[stkptr] = hi;
-				++stkptr; /* save big recursion for later */
-			}
-
-			if (lo < higuy) {
-				hi = higuy;
-				goto recurse; /* do small recursion */
+			} else {
+				/* We save the bigger partition (the upper one) for later. */
+				if (high > pivot) {
+					low_stack[stacks_size] = pivot + item_size;
+					high_stack[stacks_size] = high;
+					stacks_size++;
+				}
+				if (low < pivot) {
+					high = pivot - item_size;
+					goto recurse;
+				}
 			}
 		}
 	}
 
-	/* We have sorted the array, except for any pending sorts on the stack.
-	   Check if there are any, and do them. */
-
-	--stkptr;
-	if (stkptr >= 0) {
-		lo = lostk[stkptr];
-		hi = histk[stkptr];
-		goto recurse; /* pop subarray from stack */
+	/* Process what is left in the stack. */
+	if (stacks_size) {
+		stacks_size--;
+		low = low_stack[stacks_size];
+		high = high_stack[stacks_size];
+		goto recurse;
 	}
-	else
-		return RT_OK; /* all subarrays done */
+
+	ret = RT_OK;
+free:
+	return ret;
+
+error:
+	ret = RT_FAILED;
+	goto free;
 }
--- samples/tests/src/auto/zz_test_quick_sort.c
+++ samples/tests/src/auto/zz_test_quick_sort.c
@@ -2,7 +2,61 @@
 
 #include "zz_utils.h"
 
-static rt_s zz_test_quick_sort_do(struct rt_heap *heap)
+static rt_s zz_test_quick_sort_simple_callback(const void *item1, const void *item2, RT_UNUSED void *context, rt_n *comparison_result)
+{
+	rt_char8 *char1 = (rt_char8*)item1;
+	rt_char8 *char2 = (rt_char8*)item2;
+
+	*comparison_result = *char1 - *char2;
+
+	return RT_OK;
+}
+
+static rt_s zz_test_quick_sort_simple()
+{
+	rt_char8 data[20];
+	rt_un i;
+	rt_s ret;
+
+	data[0] = 15;
+	data[1] = 11;
+	data[2] = 3;
+	data[3] = 5;
+	data[4] = 19;
+	data[5] = 2;
+	data[6] = 1;
+	data[7] = 18;
+	data[8] = 10;
+	data[9] = 17;
+	data[10] = 20;
+	data[11] = 9;
+	data[12] = 4;
+	data[13] = 12;
+	data[14] = 14;
+	data[15] = 8;
+	data[16] = 6;
+	data[17] = 7;
+	data[18] = 13;
+	data[19] = 16;
+
+	if (RT_UNLIKELY(!rt_quick_sort(data, 20, 1, &zz_test_quick_sort_simple_callback, RT_NULL)))
+		goto error;
+	
+	for (i = 0; i < 20; i++) {
+		if (data[i] != (rt_char8)i + 1)
+			goto error;
+	}
+
+	ret = RT_OK;
+free:
+	return ret;
+
+error:
+	ret = RT_FAILED;
+	goto free;
+}
+
+static rt_s zz_test_quick_sort_random(struct rt_heap *heap)
 {
 	rt_un *array = RT_NULL;
 	rt_un size;
@@ -60,7 +114,8 @@ rt_s zz_test_quick_sort(void)
 		goto error;
 	runtime_heap_created = RT_TRUE;
 
-	if (RT_UNLIKELY(!zz_test_quick_sort_do(&runtime_heap.heap))) goto error;
+	if (RT_UNLIKELY(!zz_test_quick_sort_simple())) goto error;
+	if (RT_UNLIKELY(!zz_test_quick_sort_random(&runtime_heap.heap))) goto error;
 
 	ret = RT_OK;
 free:
